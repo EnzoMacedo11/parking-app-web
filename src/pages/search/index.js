f981d277397d85102356a9cd9dfcd78b84dc0b1a
EnzoMacedo11 <enzomacedo15@gmail.com>
2023-11-01 16:48:21 -0300

    v3 test qr code reader

--- src/pages/search/index.js
+++ src/pages/search/index.js
@@ -26,9 +26,10 @@ export default function Search() {
   //   }
   // }, [userData]);
 
-  useEffect(()=>{ function onScanSuccess(decodedText, decodedResult) {
+  useEffect(()=>{ 
+    function onScanSuccess(decodedText, decodedResult) {
     // handle the scanned code as you like, for example:
-    console.log(`Code matched = ${decodedText}`, decodedResult);
+    alert(`Code matched = ${decodedText}`, decodedResult);
   }
   
   function onScanFailure(error) {
