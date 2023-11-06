a8a881d1e15b4031649e789c48268b4e92dc9b90
EnzoMacedo11 <enzomacedo15@gmail.com>
2023-11-06 17:42:43 -0300

    v1 test - fix: scale qrcode scanner camera

--- src/pages/search/index.js
+++ src/pages/search/index.js
@@ -35,7 +35,7 @@ export default function Search() {
 
         let html5QrcodeScanner = new Html5QrcodeScanner(
           "reader",
-          { fps: 10, qrbox: { width: 250, height: 250 } },
+          { fps: 10, qrbox: { width: 220, height: 220 } },
           /* verbose= */ false
         );
         html5QrcodeScanner.render(onScanSuccess, onScanFailure);
